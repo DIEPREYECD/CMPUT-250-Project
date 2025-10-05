f8277756e2901d420e8f29c0b6c13abbd8d21a16
m.lee69 <m.lee69@share.epsb.ca>
2025-10-04 19:36:45 -0600

    Added the AudioController gameObject

---
+++ Streamer University/Assets/Scripts/Game/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController _instance;
+    public static AudioController Instance { get { return _instance; } }
+
+    public AudioSource audioSource;
+    public AudioClip bgm, chooseEvent;
+
+    private void Awake()
+    {
+        _instance = this;
+        toggleBGM();
+    }
+
+    public void toggleBGM()
+    {
+        audioSource.PlayOneShot(bgm);
+    }
+
+    public void PlayChooseEvent()
+    {
+        audioSource.PlayOneShot(chooseEvent);
+    }
+}
