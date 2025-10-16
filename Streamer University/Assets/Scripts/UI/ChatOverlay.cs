d3a89838d3b0f76c8b1e71021209401bf0a9c221
Ali Lathey <aelathy@ualberta.ca>
2025-10-16 12:59:49 -0600

    Fixed Scroll

--- Streamer University/Assets/Scripts/UI/ChatOverlay.cs
+++ Streamer University/Assets/Scripts/UI/ChatOverlay.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatOverlay : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class ChatOverlay : MonoBehaviour
 
     [SerializeField] private Transform content;
     [SerializeField] private ChatMessageItem itemPrefab;
+    [SerializeField] private ScrollRect scroll;
     public int maxItems = 15;
+    private bool autoScroll = true;
 
     private readonly Queue<ChatMessageItem> active = new Queue<ChatMessageItem>();
 
@@ -27,9 +30,11 @@ public class ChatOverlay : MonoBehaviour
     public void Update()
     {
         // Make the scroll rect go to bottom when new messages come in
-        var scroll = GetComponent<UnityEngine.UI.ScrollRect>();
         if (scroll.verticalNormalizedPosition <= 0.15f)
-            scroll.verticalNormalizedPosition = 0f;
+            autoScroll = true;
+        else {
+            autoScroll = false;
+        }
     }
 
     public void Push(string user, string message)
@@ -43,6 +48,16 @@ public class ChatOverlay : MonoBehaviour
         item.Set(user, message);
 
         active.Enqueue(item);
+        if (autoScroll) {
+            StartCoroutine(ScrollBottom());
+        }
+    }
+
+    private IEnumerator ScrollBottom() {
+        yield return new WaitForEndOfFrame();
+
+        Canvas.ForceUpdateCanvases();
+        scroll.verticalNormalizedPosition = 0f;
     }
 
     public void SetActive(bool active)
