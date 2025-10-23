46cae5ee3ce03ca575c11a78be94518a7382e559
Ali Lathey <aelathy@ualberta.ca>
2025-10-22 20:07:04 -0600

    Added progressively increasing chat rate with fame

--- Streamer University/Assets/Scripts/UI/ChatTester.cs
+++ Streamer University/Assets/Scripts/UI/ChatTester.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class ChatTester : MonoBehaviour
 {
-    [SerializeField] private float chatRate = 1f;
+    public PlayerStatsSO playerStats;
+    [SerializeField] private float chatRate;
 
     void Start()
     {
         StartCoroutine(SimulateChat());
     }
 
+
     private IEnumerator SimulateChat() {
         while (true) {
+            chatRate = 30f / Mathf.Max(1f, Mathf.Pow((playerStats.Fame * 0.4f), 1.5f));
             yield return new WaitForSeconds(chatRate);
-
 
             ChatBarkSystem.Instance.PushBark();
         }
