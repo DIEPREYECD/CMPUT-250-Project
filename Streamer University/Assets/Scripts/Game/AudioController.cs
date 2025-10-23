221cc7bf034adfbdd20fa544bfaa085a3dd8effe
MatthewLTY <tszyu1@ualberta.ca>
2025-10-22 19:39:35 -0600

    Modified audioController.cs so that it now does not require external code to play soundtracks, also added audioController object to other scenes

--- Streamer University/Assets/Scripts/Game/AudioController.cs
+++ Streamer University/Assets/Scripts/Game/AudioController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioController : MonoBehaviour
 {
@@ -13,6 +14,25 @@ public class AudioController : MonoBehaviour
     private void Awake()
     {
         _instance = this;
+        checkScene();
+    }
+
+    private void checkScene()
+    {
+        // Get the name of the current scene
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "StreamScene")
+        {
+            Debug.Log("play stream bgm");
+        }
+        else if (sceneName == "MainMenu")
+        {
+            Debug.Log("play main menu bgm");
+        }
+        else if (sceneName == "GameWin" || sceneName == "GameOver");
+        {
+            Debug.Log("play game over bgm");
+        }
     }
     
     public void toggleBGM(string scene)
