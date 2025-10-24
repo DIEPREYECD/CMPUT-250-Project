52f24527fafacafca46be2ad11e53ef914b5e6fb
Ali Lathey <aelathy@ualberta.ca>
2025-10-24 13:48:54 -0600

    Chat frequency changes added

--- Streamer University/Assets/Scripts/UI/ChatTester.cs
+++ Streamer University/Assets/Scripts/UI/ChatTester.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChatTester : MonoBehaviour
 {
-    [SerializeField] private float chatRate = 1f;
+    [SerializeField] private float chatRate;
 
     void Start()
     {
@@ -13,6 +13,11 @@ public class ChatTester : MonoBehaviour
 
     private IEnumerator SimulateChat() {
         while (true) {
+            if (PlayerController.Instance.Fame != 0) {
+                chatRate = 30f / Mathf.Pow(PlayerController.Instance.Fame * 0.5f, 1.1f); // NEW - Random Calculation - if prevents div by 0.
+            } else {
+                 chatRate = 1000f; // If fame is 0
+            }
             yield return new WaitForSeconds(chatRate);
 
 
