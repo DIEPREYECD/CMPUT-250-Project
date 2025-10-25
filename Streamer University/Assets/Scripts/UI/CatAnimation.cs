a6ca958c7fe0f0b85f3055a700c510054c3197a8
tuckerostapovitch <kdostapo@ualberta.ca>
2025-10-24 22:30:50 -0600

    Cat animates, storylet changes

---
+++ Streamer University/Assets/Scripts/UI/CatAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimation : AnimatedEntity
+{
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimationSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimationUpdate();
+      
+    }
+}
